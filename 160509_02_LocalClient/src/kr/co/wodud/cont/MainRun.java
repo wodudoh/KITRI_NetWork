eb7329382508442c316c6f95f02795e6d7c57894
오재영 <wodud.oh@gmail.com>
2016-05-09 11:16:40 +0900

    Client Socket 제작중
    Signed-off-by: 오재영 <wodud.oh@gmail.com>

--- 160509_02_LocalClient/src/kr/co/wodud/cont/MainRun.java
+++ 160509_02_LocalClient/src/kr/co/wodud/cont/MainRun.java
@@ -2,25 +2,16 @@ package kr.co.wodud.cont;
 
 import java.io.IOException;
 import java.net.InetAddress;
-import java.net.InetSocketAddress;
-import java.net.ServerSocket;
+import java.net.Socket;
 import java.net.UnknownHostException;
 
-/**
- * Server 프로그래밍
- * 
+/** Client 프로그래밍 
  * @author Administrator
  *
  */
 public class MainRun {
 
-	/**
-	 * Socket 프로그래밍
-	 * 
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// ip 주소값 세팅
 		InetAddress _ia = null; // 내가 지정한 변수구나 아는 방법 _언더바를 활용.
 
 		try {
@@ -36,33 +27,24 @@ public class MainRun {
 		// port : 같은 내부 아이피로부터 어떤 작업을 수행할지 번호로써 구분해줄수 있는 구분 값. 0~65535 설정 가능
 		// well-known port : 일반적으로 예약되어있는 포트 번호
 		// 60606 숫자 타입
-		int _userPort = 60606;
-
-		// Server Socket 구성
-		// 1057page
-		ServerSocket _userSS = null;
+		int _userPort = 60600;
+		// Socket API 서버에 접근할 수 있도록 구성
+		// 서버 주소 및 포트 값 지정
+		String _serverIP = "192.168.14.31";
+		int _serverPort  = 60606;
+		
+		Socket _socket = null; 
+		
 		try {
-			_userSS = new ServerSocket();
-			// Socket 구성시 필요 요소 : IP, Port
-			// 1058page Server Socket.bind
-			_userSS.bind(new InetSocketAddress(_ia.getHostAddress(), _userPort)); // endpoint 는 inetSockAdress
-			
-			int count =0;
-			boolean a = true;
-			while(a){
-			//구성된 ServerSocket Open
-			_userSS.accept();
-			count++;
-			if(count >= 10){
-				 a = false;
-				}
-			System.out.println(count+"번째 누군가 접근 함");
-			}
+			//접근하고자 하는 Server IP, Port 세팅
+			_socket = new Socket(_serverIP, _serverPort);
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
-		
+		} 
 
 	}
 
